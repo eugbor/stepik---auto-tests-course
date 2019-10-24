5c219eb01468ac6cba4d2f22e9dcbdab403e7066
John Doe <johndoe@example.com>
2019-10-24 09:25:33 +0300

    init and test_main_page added

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language")
 
 @pytest.fixture(scope="function")
@@ -12,7 +12,7 @@ def browser(request):
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print("\nstart browser for test..")
-    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome(executable_path="/usr/lib/chromium-browser/chromedriver", options=options)
     browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
